5ba7e3ec8b00c3e7afb7508d8847d3153b590871
yinyong <yanni4night@gmail.com>
2016-06-21 17:46:11 +0800

    complete

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+  * Copyright (C) 2016 pantojs.xyz
+  * index.js
+  *
+  * changelog
+  * 2016-06-21[17:41:48]:revised
+  *
+  * @author yanni4night@gmail.com
+  * @version 1.0.0
+  * @since 1.0.0
+  */
+'use strict';
+
+module.exports = 'pantojs is coming soon';
